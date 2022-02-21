6362d65073396fb30d7fe4ba5d9d2417efd19d03
Belen Nazareth <belennazareth29@gmail.com>
2022-02-21 12:43:59 +0100

    Añadido el fichero zoo para el proyecto

--- zoobbdd.sql
+++ zoobbdd.sql
@@ -3,29 +3,27 @@ create table empleados
 DNI VARCHAR (9),
 Nombre VARCHAR (30),
 Fechacontrato DATE,
-Exhibicioncodigo INT (5),
-CONSTRAINT PK_empleados PRIMARY KEY (DNI),
-CONSTRAINT FK_empleadosex FOREIGN KEY (Exhibicioncodigo) REFERENCES exhibicion (Codigo)
+ExhibicionNombre VARCHAR (15),
+CONSTRAINT PK_empleados PRIMARY KEY (DNI)
 );
 
 create table animales
 (
-ID INT (4),
+ID VARCHAR (4),
 Nombre VARCHAR (20),
 Especie VARCHAR (20),
-Exhibicioncodigo INT (5),
-CONSTRAINT PK_animales PRIMARY KEY (ID),
-CONSTRAINT FK_animalesex FOREIGN KEY (Exhibicioncodigo) REFERENCES exhibicion (Codigo)
+ExhibicionNombre VARCHAR (15),
+CONSTRAINT PK_animales PRIMARY KEY (ID)
 );
 
 create table exhibicion
 (
-Codigo INT (5),
+Codigo VARCHAR (5),
 Nombre VARCHAR (15),
 Duracion TIME,
-AnimalID INT (4),
+AnimalID VARCHAR (4),
 EmpleadoDNI VARCHAR (9),
-CONSTRAINT PK_exhibicion PRIMARY KEY,
+CONSTRAINT PK_exhibicion PRIMARY KEY (Codigo),
 CONSTRAINT FK_exhibicionan FOREIGN KEY (AnimalID) REFERENCES animales (ID),
-CONSTRAINT FK_exhibicionemp FOREIGN KEY (EmpleadoDNI) FOREIGN KEY empleados (DNI)
+CONSTRAINT FK_exhibicionemp FOREIGN KEY (EmpleadoDNI) REFERENCES empleados (DNI)
 );
